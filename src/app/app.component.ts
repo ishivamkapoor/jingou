7a5ad9a065f23508d05040a99128ba0cd10649fd
DESKTOP-BEDR0QT\mails <shivam@offshoresolutions.nl>
2018-12-30 20:18:29 +0530

    Navigation Bar Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,18 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string,icon:string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Menu', icon:'menu-icon.png', component: HomePage },
+      { title: 'My Orders',icon:'order-icon.png', component: HomePage },
+      { title: 'Offers', icon:'offer-icon.png', component: HomePage },
+      { title: 'Support', icon:'support-icon.png', component: HomePage },
+      { title: 'Support', icon:'settings-icon.png', component: HomePage }
     ];
 
   }
